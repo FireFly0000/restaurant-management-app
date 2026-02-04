1641d78f5ff33b35d86b0d528bb56aea89c5be45
tdat7749 <hyperiondat@gmail.com>
2026-02-04 21:30:47 +0700

    [feat/RES001] - Enable Service Discovery, gRPC Dubbo, Kafka for services.

--- businessservice/src/main/java/com/restaurant/businessservice/BusinessServiceApplication.java
+++ businessservice/src/main/java/com/restaurant/businessservice/BusinessServiceApplication.java
@@ -1,9 +1,15 @@
 package com.restaurant.businessservice;
 
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableDubbo
+@EnableKafka
+@EnableDiscoveryClient
 public class BusinessServiceApplication {
 
     public static void main(String[] args) {
