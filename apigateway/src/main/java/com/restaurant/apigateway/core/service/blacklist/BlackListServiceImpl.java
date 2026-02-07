5e15c7ab48a39436ed30c52a990a2de88575d122
Bang Do <bang.do@xiontechs.com>
2026-02-07 11:20:15 +0700

    Added blacklist and valiate blacklisted token

--- apigateway/src/main/java/com/restaurant/apigateway/core/service/blacklist/BlackListServiceImpl.java
+++ apigateway/src/main/java/com/restaurant/apigateway/core/service/blacklist/BlackListServiceImpl.java
@@ -1,7 +1,32 @@
 package com.restaurant.apigateway.core.service.blacklist;
 
+import com.restaurant.apigateway.core.service.redis.IRedisService;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
-public class BlackListServiceImpl {
+public class BlackListServiceImpl implements IBackListService {
+    private static final String PREFIX = "blacklist:jwt:";
+
+    private final IRedisService redisService;
+
+    public BlackListServiceImpl(IRedisService redisService) {
+        this.redisService = redisService;
+    }
+
+    @Override
+    public boolean isBlacklisted(String token) {
+        return redisService.hasKey(PREFIX + token);
+    }
+
+    @Override
+    public void blacklistToken(String token, long ttlSeconds) {
+        redisService.setWithTTL(
+                PREFIX + token,
+                true,
+                ttlSeconds,
+                TimeUnit.SECONDS
+        );
+    }
 }
--- apigateway/src/main/java/com/restaurant/apigateway/core/service/blacklist/IBackListService.java
+++ apigateway/src/main/java/com/restaurant/apigateway/core/service/blacklist/IBackListService.java
@@ -1,4 +1,6 @@
 package com.restaurant.apigateway.core.service.blacklist;
 
 public interface IBackListService {
+    boolean isBlacklisted(String token);
+    void blacklistToken(String token, long ttlSeconds);
 }
