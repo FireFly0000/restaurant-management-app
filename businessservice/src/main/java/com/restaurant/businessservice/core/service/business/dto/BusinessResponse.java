7f1df67442a7446d6db325a9305faf5506a1d582
tdat7749 <hyperiondat@gmail.com>
2026-03-04 21:40:59 +0700

    [feat/RES303] - Add MapperUtils and create endpoint for create function

--- businessservice/src/main/java/com/restaurant/businessservice/core/service/business/dto/BusinessResponse.java
+++ businessservice/src/main/java/com/restaurant/businessservice/core/service/business/dto/BusinessResponse.java
@@ -1,4 +1,22 @@
 package com.restaurant.businessservice.core.service.business.dto;
 
+import lombok.Builder;
+
+import java.util.Date;
+
+@Builder
 public class BusinessResponse {
+    private String id;
+    private String name;
+    private String businessType;
+    private String description;
+    private String avatarUrl;
+    private String coverImgUrl;
+    private String phoneNumber;
+    private String email;
+    private String websiteUrl;
+    private Boolean isActive;
+    private Integer locationCount;
+    private Date createdAt;
+    private Date updatedAt;
 }
