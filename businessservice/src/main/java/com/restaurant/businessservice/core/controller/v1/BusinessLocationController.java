6b33f1033c4e1c95ce0745e44e7b1589fc379d59
tdat7749 <hyperiondat@gmail.com>
2026-04-07 22:06:25 +0700

    [feat/RES309] - Controller for functions

--- businessservice/src/main/java/com/restaurant/businessservice/core/controller/v1/BusinessLocationController.java
+++ businessservice/src/main/java/com/restaurant/businessservice/core/controller/v1/BusinessLocationController.java
@@ -5,6 +5,8 @@ import com.restaurant.businessservice.common.MsgUtil;
 import com.restaurant.businessservice.core.service.location.IBusinessLocationService;
 import com.restaurant.businessservice.core.service.location.dto.BusinessLocationResponse;
 import com.restaurant.businessservice.core.service.location.dto.CreateLocationRequest;
+import com.restaurant.businessservice.core.service.location.dto.UpdateLocationManagerRequest;
+import com.restaurant.businessservice.core.service.location.dto.UpdateLocationRequest;
 import com.restaurant.businessservice.model.Location;
 import com.restaurant.commons.core.dtos.ApiResponse;
 import com.restaurant.commons.utils.AppUtils;
@@ -15,6 +17,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/business-locations")
@@ -36,7 +39,48 @@ public class BusinessLocationController {
     public ResponseEntity<?> create(@RequestBody @Valid CreateLocationRequest request){
         Location location = this._locationService.create(request);
         BusinessLocationResponse response = MapperUtils.getLocationResponse(location, new HashMap<>());
-        ApiResponse apiResponse = AppUtils.buildResponse(_msgUtil.getMessage("location.create.success"),response, null);
+        ApiResponse apiResponse = AppUtils.buildResponse(_msgUtil.getMessage("location.create.success"), response, null);
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}", consumes = "application/json", produces = "application/json")
+    @PreAuthorize("@_authorizer.isOwner(#id, T(com.restaurant.commons.constant.Entity).BUSINESS)")
+    public ResponseEntity<?> update(@PathVariable("id") String id, @RequestBody @Valid UpdateLocationRequest request){
+        UUID locationId = UUID.fromString(id);
+        Location location = this._locationService.update(locationId, request);
+        BusinessLocationResponse response = MapperUtils.getLocationResponse(location, new HashMap<>());
+        ApiResponse apiResponse = AppUtils.buildResponse(_msgUtil.getMessage("location.update.success"), response, null);
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}/active", produces = "application/json")
+    @PreAuthorize("@_authorizer.isOwner(#id, T(com.restaurant.commons.constant.Entity).BUSINESS)")
+    public ResponseEntity<?> active(@PathVariable("id") String id){
+        UUID locationId = UUID.fromString(id);
+        Boolean result = this._locationService.active(locationId);
+        ApiResponse apiResponse = AppUtils.buildResponse(_msgUtil.getMessage("location.active.success"), result, null);
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}/inactive", produces = "application/json")
+    @PreAuthorize("@_authorizer.isOwner(#id, T(com.restaurant.commons.constant.Entity).BUSINESS)")
+    public ResponseEntity<?> inactive(@PathVariable("id") String id){
+        UUID locationId = UUID.fromString(id);
+        Boolean result = this._locationService.inactive(locationId);
+        ApiResponse apiResponse = AppUtils.buildResponse(_msgUtil.getMessage("location.inactive.success"), result, null);
+
+        return new ResponseEntity<>(apiResponse, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{id}/manager", consumes = "application/json", produces = "application/json")
+    @PreAuthorize("@_authorizer.isOwner(#request.businessId, T(com.restaurant.commons.constant.Entity).BUSINESS)")
+    public ResponseEntity<?> changeManager(@PathVariable("id") String id, @RequestBody @Valid UpdateLocationManagerRequest request){
+        UUID locationId = UUID.fromString(id);
+        Boolean result = this._locationService.changeManager(locationId, request);
+        ApiResponse apiResponse = AppUtils.buildResponse(_msgUtil.getMessage("location.change_manager.success"), result, null);
 
         return new ResponseEntity<>(apiResponse, HttpStatus.OK);
     }
